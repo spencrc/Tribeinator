ed09d22da760c1e245604f31e7efd049eb0cb4c8
spencerishere <145073726+spencerishere@users.noreply.github.com>
2025-07-05 14:40:46 -0700

    added helpful msg for incorrect docker-push.sh use

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -4,6 +4,16 @@
 username=$1
 repo_name=$2
 
+if [ $# -ne 2 ]; then
+    echo -e "Usage: $0 <username> <repo_name>\n"
+    echo -n -e "Description:\n\tThis script pushes a Docker image to Docker Hub. " 
+    echo -e "You are\n\trequired to be logged into Docker to use this script!\n"
+    echo -e "Arguments:\n\tusername\tYour Docker Hub username"
+    echo -e "\trepo_name\tThe name of the repository you wish to push to\n"
+    echo -e "Example:\n\t$0 spencrc tribeinator"
+    exit 1
+fi
+
 # Finds the current directory, and spits out its name in lowercases
 directory=${PWD##*/}
 source_image=$(basename $directory | awk '{print tolower($0)}')
