46fedb139528f342f1ecf37ce95fd600035f086b
spencerishere <145073726+spencerishere@users.noreply.github.com>
2025-07-08 19:43:56 -0700

    now checks to see if user has the role they want to remove

--- src/commands/slash/removeselfrole.ts
+++ src/commands/slash/removeselfrole.ts
@@ -11,25 +11,33 @@ export default class implements SlashCommand {
 				.setDescription('The role name')
 				.setRequired(true)
 		)
-	public execute(interaction: ChatInputCommandInteraction): void {
+	public async execute(interaction: ChatInputCommandInteraction): Promise<void> {
 		const role_name: string = interaction.options.getString('role') as string;
 		const member: GuildMember = interaction.member as GuildMember;
 		const guild: Guild = interaction.guild as Guild;
-		pgClient.query(`SELECT role_name FROM guild_roles WHERE guild_id=$1 AND "role_name"=$2;`, [+guild.id, role_name], async (err, result) =>{
-			const rowCount: number = result.rowCount as number;
-			if (!err && rowCount > 0) {
-				const role: Role = guild.roles.cache.find(role => role.name === role_name) as Role;
-				member.roles.remove(role);
-				await interaction.reply({
-					content: `Found the \`${role_name}\` role and removed it from <@${member.id}>!`,
-					flags: MessageFlags.Ephemeral
-				});
-			} else {
-				await interaction.reply({
-					content: `Sorry <@${member.id}>! Couldn't find the \`${role_name}\` role!`,
-					flags: MessageFlags.Ephemeral
-				});
-			}
-		});
+
+		if (member.roles.cache.has(role_name)) {
+			pgClient.query(`SELECT role_name FROM guild_roles WHERE guild_id=$1 AND "role_name"=$2;`, [+guild.id, role_name], async (err, result) =>{
+				const rowCount: number = result.rowCount as number;
+				if (!err && rowCount > 0) {
+					const role: Role = guild.roles.cache.find(role => role.name === role_name) as Role;
+					member.roles.remove(role);
+					await interaction.reply({
+						content: `Found the \`${role_name}\` role and removed it from <@${member.id}>!`,
+						flags: MessageFlags.Ephemeral
+					});
+				} else {
+					await interaction.reply({
+						content: `Sorry <@${member.id}>! Couldn't find the \`${role_name}\` role!`,
+						flags: MessageFlags.Ephemeral
+					});
+				}
+			});
+		} else {
+			await interaction.reply({
+				content: `Sorry <@${member.id}>! You don't have the \`${role_name}\` role!`,
+				flags: MessageFlags.Ephemeral
+			});
+		}
 	}
 };
