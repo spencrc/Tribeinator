e50ea559b789d6428f344805f3390d4b62560d7a
spencerishere <145073726+spencerishere@users.noreply.github.com>
2025-07-09 17:35:17 -0700

    SlashCommand interface now has optional autocomplete field

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -4,7 +4,8 @@ import type {
     ContextMenuCommandBuilder, 
     SlashCommandSubcommandsOnlyBuilder, 
     ChatInputCommandInteraction,
-    SlashCommandOptionsOnlyBuilder
+    SlashCommandOptionsOnlyBuilder,
+    AutocompleteInteraction
 } from "discord.js";
 
 export interface MessageCommand {
@@ -15,5 +16,6 @@ export interface MessageCommand {
 
 export interface SlashCommand {
     data: SlashCommandBuilder | ContextMenuCommandBuilder | SlashCommandSubcommandsOnlyBuilder | SlashCommandOptionsOnlyBuilder
+    autocomplete?: (interaction: AutocompleteInteraction) => Promise<void> | void
     execute: (interaction: ChatInputCommandInteraction) => Promise<void> | void
 }
