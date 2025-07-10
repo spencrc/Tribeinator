384b779ef54bda19a751e34b5016369b3cbd7a40
spencerishere <145073726+spencerishere@users.noreply.github.com>
2025-07-09 17:34:37 -0700

    interaction-create.ts can now handle autocomplete

--- src/events/interaction-create.ts
+++ src/events/interaction-create.ts
@@ -1,27 +1,45 @@
-import { Events, Interaction, CacheType, MessageFlags } from "discord.js";
+import { Events, Interaction, CacheType, MessageFlags, ChatInputCommandInteraction, AutocompleteInteraction } from "discord.js";
 import { client } from "../client.js"
+
+const executeChatInputCommand = async (interaction: ChatInputCommandInteraction<CacheType>): Promise<void> => {
+    const command = client.commands.get(interaction.commandName);
+        
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+        }
+    }
+}
+
+const autocomplete = async (interaction: AutocompleteInteraction<CacheType>): Promise<void> => {
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.autocomplete!(interaction);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 export default {
     name: Events.InteractionCreate,
-    async execute(interaction: Interaction<CacheType>) {
-        if (!interaction.isChatInputCommand()) return;
-        
-            const command = client.commands.get(interaction.commandName);
-        
-            if (!command) {
-                console.error(`No command matching ${interaction.commandName} was found.`);
-                return;
-            }
-        
-            try {
-                await command.execute(interaction);
-            } catch (error) {
-                console.error(error);
-                if (interaction.replied || interaction.deferred) {
-                    await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-                } else {
-                    await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-                }
-            }
+    execute(interaction: Interaction<CacheType>): void {
+        if ( interaction.isChatInputCommand() ) executeChatInputCommand(interaction);
+        else if ( interaction.isAutocomplete() ) autocomplete(interaction);
     }
 }
