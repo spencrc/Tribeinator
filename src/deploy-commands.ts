859cce2bb6a3b1c245fd496c09007192df8d565e
spencerishere <145073726+spencerishere@users.noreply.github.com>
2025-07-11 16:02:57 -0700

    deploy-commands.ts now relies on client.load method

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,32 +1,9 @@
 import { REST, Routes } from "discord.js";
 import { DISCORD_CLIENT_ID, DISCORD_TOKEN } from "./config.js";
-import { fileURLToPath, pathToFileURL } from 'node:url';
-import fs from "node:fs";
-import path from "node:path";
-import { SlashCommand } from "./classes/slash-command.js";
+import { client } from "./client.js";
 
-const commands: SlashCommand[] = [];
-const __filename: string = fileURLToPath(import.meta.url); //gets this file's name
-const __dirname: string = path.dirname(__filename); //gets this file's directory name
-const foldersPath: string = path.join(__dirname, "commands");
-const commandFolders: string[] = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-    const commandsPath: string = path.join(foldersPath, folder);
-
-    if (!fs.statSync(commandsPath).isDirectory()) continue; //ensures that commandsPath is really a directory, not a ".js" file
-
-    const commandFiles: string[] = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js')); //gets all files, and ensures they end with ".js"
-
-    for (const file of commandFiles) {
-        const filePath: string = path.join(commandsPath, file);
-        const commandClass = (await import( pathToFileURL(filePath).href ))
-            .default
-        const command = new commandClass();
-        
-        commands.push(command.data.toJSON());
-    }
-}
+// Loads all commands
+client.load();
 
 const rest: REST = new REST().setToken(DISCORD_TOKEN);
 
@@ -34,10 +11,10 @@ const rest: REST = new REST().setToken(DISCORD_TOKEN);
 	try {
 		console.log(`Started refreshing application (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
+		// The put method is used to fully refresh all commands globally with the current set
 		await rest.put(
 			Routes.applicationCommands(DISCORD_CLIENT_ID),
-			{ body: commands },
+			{ body: client.commands },
 		);
 
 		console.log(`Successfully reloaded application (/) commands.`);
