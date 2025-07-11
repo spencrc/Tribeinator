204f0f84d57cae4e0ca5cb5eff4885f22bf5415b
spencerishere <145073726+spencerishere@users.noreply.github.com>
2025-07-11 03:14:20 -0700

    removed DISCORD_GUILD_ID from config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,6 @@ dotenv.config();
 
 export const DISCORD_TOKEN: string = process.env.DISCORD_TOKEN as string;
 export const DISCORD_CLIENT_ID: string = process.env.DISCORD_CLIENT_ID as string;
-export const DISCORD_GUILD_ID: string = process.env.DISCORD_GUILD_ID as string;
 
 export const POSTGRES_HOST: string = process.env.POSTGRES_HOST as string;
 export const POSTGRES_USER: string = process.env.POSTGRES_USER as string;
