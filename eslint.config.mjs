1e451e3ac18b459980e49c2bf46ae62408e6ef90
spencerishere <145073726+spencerishere@users.noreply.github.com>
2025-07-18 02:32:34 -0700

    changed config preset to strictTypeChecked

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.strictTypeChecked,
   {
         languageOptions: {
             parserOptions: {
